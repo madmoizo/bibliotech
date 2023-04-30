4c69356c5c1317c9c91f9fe584288bae4407db8d
madmoizo <matthieu.dambrune@gmail.com>
2023-04-30 23:48:41 +0000

    feat(blobToFileList): add `filename` param

--- src/blobToFileList.js
+++ src/blobToFileList.js
@@ -1,10 +1,11 @@
 /**
  * Convert a blob to a FileList
  * @param {Blob} blob
+ * @param {string} [filename='unknown']
  * @returns {FileList}
  */
-export default function blobToFileList (blob) {
-  const file = new File([blob], 'unknown', { type: blob.type })
+export default function blobToFileList (blob, filename='unknown') {
+  const file = new File([blob], filename, { type: blob.type })
   const dataTransfer = new DataTransfer()
   dataTransfer.items.add(file)
   return dataTransfer.files
