c478f314fca25999d5569df70a967dc139d0e5b5
madmoizo <matthieu.dambrune@gmail.com>
2023-04-30 23:49:46 +0000

    refactor(blobToFileList): one liner

--- src/capitalizeFirstLetter.js
+++ src/capitalizeFirstLetter.js
@@ -4,9 +4,5 @@
  * @returns {string}
  */
 export default function capitalizeFirstLetter (str) {
-  if (str.length <= 1) {
-    return str.charAt(0).toUpperCase()
-  } else {
-    return `${str.charAt(0).toUpperCase()}${str.slice(1)}`
-  }
+  return `${str.charAt(0).toUpperCase()}${str.slice(1)}`
 }
